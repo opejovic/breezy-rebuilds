7d1fb17e70a2ef30b749f66e495a49dee5359627
Ognjen <opejovic@gmail.com>
2021-08-10 20:07:59 +0200

    initial commit with stripe tailwind rebuild

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <StripeLanding />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import StripeLanding from './components/AppStripeLanding.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    StripeLanding
+  }
+})
+</script>
+
+<style>
+
+</style>
